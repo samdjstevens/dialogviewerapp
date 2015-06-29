5e5c75853469bc55823eaac72b506b694734012d
Sam Stevens <sam@ticketline.co.uk>
2015-06-29 23:14:04 +0100

    add in some dummy data and implement the basic collection view display methods

--- DialogViewer/ViewController.swift
+++ DialogViewer/ViewController.swift
@@ -8,18 +8,97 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+
+    private var sections: [[String: String]]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Define the sections
+        sections = [
+        
+            [
+                "header": "First Witch",
+                "content": "Hey, when will the three of us meet up later?"
+            ],
+            [
+                "header": "Second Witch",
+                "content": "When everything's straightened out."
+            ],
+            [
+                "header": "Third Witch",
+                "content": "That'll be just before sunset."
+            ],
+            [
+                "header": "First Witch",
+                "content": "Where?"
+            ],
+            [
+                "header": "Second Witch",
+                "content": "The dirst patch."
+            ],
+            [
+                "header": "Third Witch",
+                "content": "I guess we'll see Mac there."
+            ]
+        ]
+    
+        
+        // Register the ContentCell class with the collection view
+        collectionView?.registerClass(ContentCell.self, forCellWithReuseIdentifier: "CONTENT")
+        
+        // Move the content view down from the status bar
+        var contentInset = collectionView?.contentInset
+        contentInset?.top = 20
+        collectionView?.contentInset = contentInset!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Helper method to retrieve an array(?) of words in a string.
+    func  wordsInSection(section: Int) -> [String] {
+        let content = sections[section]["content"]
+        let spaces = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        let words = content?.componentsSeparatedByCharactersInSet(spaces)
+        
+        return words!
     }
-
-
+    
+    // The number of sections to display
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    // The number of cells in a section
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return wordsInSection(section).count
+    }
+    
+    // The content of a cell
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        // The words for this section
+        let words = wordsInSection(indexPath.section)
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CONTENT", forIndexPath: indexPath) as! ContentCell
+        cell.maxWidth = collectionView.bounds.size.width
+        cell.text = words[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // Determine the size of a cell
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let words = wordsInSection(indexPath.section)
+        let size = ContentCell.sizeForContentString(words[indexPath.row], forMaxWidth: collectionView.bounds.size.width)
+        
+        return size
+    }
 }
 
